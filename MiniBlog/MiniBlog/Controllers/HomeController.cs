db32be3f6466d55e8b05444ac3ac530c38b35dcb
RogerMetzger <roger.metzger@hotmail.com>
2018-01-19 20:40:42 +0100

    Db connection added

--- MiniBlog/MiniBlog/Controllers/HomeController.cs
+++ MiniBlog/MiniBlog/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MiniBlog.Models;
 
 namespace MiniBlog.Controllers
 {
     public class HomeController : Controller
     {
+        public MiniBlogContext context = new MiniBlogContext();
         public ActionResult Index()
         {
             return View();
